9919f1458b742778bffccfea97a84ac95490badd
mythLabs <31563084+mythLabs@users.noreply.github.com>
2017-11-26 09:07:32 +0530

    add delete to play ground (delete many,delete one,find one & delete)

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,33 @@
+const {MongoClient, ObjectID} = require('mongodb'); //De-structuring
+
+const DbUrl ='mongodb://localhost:27017/TodoApp';
+MongoClient.connect(DbUrl,(err,db) => {
+    if(err) {
+       return console.log("error",err);
+    }
+    console.log("connected to mongodb");
+
+
+// delete Many
+// db.collection('Todos').deleteMany({text:'Eat lunch'}).then((result) =>{
+//     console.log(result)
+//    },(err) => {
+//      console.log('unable to delete');
+//    });
+
+//delete one
+// db.collection('Todos').deleteOne({text:'Eat lunch'}).then((result) =>{
+//     console.log(result)
+//    },(err) => {
+//      console.log('unable to delete');
+//    });
+
+//find one and delete
+db.collection('Todos').findOneAndDelete({text:'Eat lunch'}).then((result) =>{
+    console.log(result)
+   },(err) => {
+     console.log('unable to delete');
+   });
+
+//db.close();
+});
